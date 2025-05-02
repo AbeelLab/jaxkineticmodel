be4533dfcc44d1211822ed0f5ae97a32b3906c5e
Paul_v_Lent <P.H.vanLent@tudelft.nl>
2025-05-02 15:21:12 +0200

    Deployed 2328f3a with MkDocs version: 1.6.1

--- code/custom_reactions.py
+++ code/custom_reactions.py
@@ -15,9 +15,8 @@ print(v1.symbolic())
 # A*v_max/(km_A*(A/km_A + 1))
 v1.add_modifier(modifier.SimpleActivator(activator="C", k_A="k"))
 print(v1.symbolic())
-
-
 # A*v_max*(C/k + 1)/(km_A*(A/km_A + 1))
+
 
 class JaxNewMechanism(Mechanism):
     """Example of a custom reaction implementation"""
@@ -29,7 +28,6 @@ class JaxNewMechanism(Mechanism):
         This is of course a non-sense reaction"""
 
         return (substrate * (10 / 2) + 5 ** parameter1) / parameter2
-
 
 v_new = JaxNewMechanism(substrate="A",
                          parameter1="vmax",
